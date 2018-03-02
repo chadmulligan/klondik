7e6df0ae5c4030699e260d4ba0f6e5070bd06a33
chadmulligan <jeremy.isnard@gmail.com>
2018-03-02 17:41:35 +0100

    dlbutton

--- app.R
+++ app.R
@@ -33,8 +33,10 @@ ui <- fluidPage(
       actionButton(inputId = "getcluster",
                      label = "Cluster"),
       helpText("Look for parent addresses from the results"),
-      hr()
+      hr(),
       
+      downloadButton(outputId = "downloadCSV",
+                     label = "Download .csv")
       
     ),
     
@@ -42,12 +44,11 @@ ui <- fluidPage(
     mainPanel(
       tabsetPanel(id = "nav",
                   tabPanel("Transactions", DTOutput("transactions")),
-                  tabPanel("New Cluster", 
+                  tabPanel("Cluster", 
                            verbatimTextOutput("cluster"),
-                           actionButton(inputId = "getclusterinfo",
-                                        label = "Get new transactions")),
+                           uiOutput("getclusterinfo")),
                   tabPanel("All Addresses", verbatimTextOutput("allAddresses"))
-                   )
+                  )
     )
   )
 )
@@ -56,7 +57,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   hideTab(inputId = "nav", 
-          target = "New Cluster")
+          target = "Cluster")
   
   hideTab(inputId = "nav", 
           target = "All Addresses")
@@ -64,53 +65,76 @@ server <- function(input, output, session) {
   hideTab(inputId = "nav", 
           target = "Transactions")
   
-  
   values <- reactiveValues()
   
   
   observeEvent(input$getinfo, {
       
-    values$allAddresses <- input$address
-    
-    tryCatch(
-     {values$transactions <- get(input$address)
+    tryCatch({
+      values$transactions <- get(input$address)
+      output$transactions <- renderDT(DT::datatable(values$transactions, 
+                                                   options = list(lengthMenu = list(c(100, -1),
+                                                                                    c("100", 'All')))))
       showTab(inputId = "nav",
               target = "Transactions", 
-              select = TRUE)},
+              select = TRUE)
+      
+      values$allAddresses <- input$address
+      
+      values$searched <- input$address
+      
+      values$cluster <- c()
+
+      },
       error = function(e) {
         showNotification(
-          ui = "Please enter a valid BTC address",
+          ui = "Please enter a valid BTC address.",
           type = "error")}
       )
-    
-    output$transactions <- renderDT(DT::datatable(values$transactions, 
-                                                  options = list(lengthMenu = list(c(100, -1),
-                                                                                   c("100", 'All')))))
   })
   
   
   observeEvent(input$append, {
     
     tryCatch({
+      
+    if (input$address %in% values$searched) warning() else{
       values$transactions <- rbind(get(input$address), values$transactions)
       values$allAddresses <- c(input$address, values$allAddresses)
-      },
-      error = function(e) {
-        showNotification(
-          ui = "Please enter a valid BTC address",
-          type = "error")}
+      output$transactions <- renderDT(DT::datatable(values$transactions, 
+                                                    options = list(lengthMenu = list(c(100, -1),
+                                                                                     c("100", 'All')))))
+      output$allAddresses <- renderText({paste(values$allAddresses, 
+                                               collapse = "\n")})
+
+      updateTabsetPanel(session,
+                        inputId = "nav", 
+                        selected = "Transactions")
+      
+      showTab(inputId = "nav",
+              target = "All Addresses",
+              select = FALSE)
+      
+      values$searched <- c(values$searched, input$address)
+      
+      }
+    },
+    warning = function(w) {
+      showNotification(
+        ui = "You already searched this address.",
+        type = "warning")
+    },
+    error = function(e) {
+      showNotification(
+        ui = "Please enter a valid BTC address.",
+        type = "error")
+      }
     )
-    
-    output$transactions <- renderDT(DT::datatable(values$transactions, 
-                                                  options = list(lengthMenu = list(c(100, -1),
-                                                                                   c("100", 'All')))))
   })
   
 
 
   observeEvent(input$getcluster, {
-    
-    values$cluster <- c()
     
     tryCatch({
       values$cluster <- cluster(values$transactions, values$allAddresses)
@@ -124,12 +148,18 @@ server <- function(input, output, session) {
                                                collapse = "\n")})
       
       showTab(inputId = "nav",
-              target = "New Cluster", 
+              target = "Cluster", 
               select = TRUE)
-      
+        
       showTab(inputId = "nav",
               target = "All Addresses", 
               select = FALSE)
+      
+      output$getclusterinfo <- renderUI({
+        actionButton(inputId = "clusterinfobutton",
+                          label = "Get new transactions")
+        })
+      
     }, 
     error = function(err){
       showNotification(
@@ -140,15 +170,31 @@ server <- function(input, output, session) {
   })
 
   
-  observeEvent(input$getclusterinfo, {
+  observeEvent(input$clusterinfobutton, {
 
-    values$transactions <- rbind(get(values$cluster), values$transactions)
+    values$transactions <- rbind(get(values$cluster), values$transactions) 
+    
+    values$searched <- c(values$searched, values$cluster)
+    
+    values$cluster <- c()
     
     updateTabsetPanel(session,
                       inputId = "nav", 
                       selected = "Transactions")
+    
+    removeUI(selector = "#clusterinfobutton")
+    
     })
   
+  
+  output$downloadCSV <- downloadHandler(
+    filename = function() {
+      paste(paste(values$searched[[1]]), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(as.data.frame(c(values$transactions)), file)
+    }
+  )
   
 }
 
