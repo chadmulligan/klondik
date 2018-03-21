aabe3ca2c647c7913ac9292fbf4b105524495a9d
chadmulligan <jeremy.isnard@gmail.com>
2018-03-21 11:32:43 +0100

    updatePrices() folder issue

--- shinyApp/global.R
+++ shinyApp/global.R
@@ -3,12 +3,12 @@
 ###UPDATING THE PRICES DF###
 updatePrices <- function()  {
   
-  lastmodif <- format(file.info("www/data/prices.RData")$mtime,
+  lastmodif <- format(file.info("./www/data/prices.RData")$mtime,
                       format = "%Y-%m-%d")
   if(lastmodif != Sys.Date()) {
-  token = readRDS("www/data/token.RDS")
+  token = readRDS("./www/data/token.RDS")
   drop_download(path = "btc-prices/prices.Rdata",
-                local_path = "www/data/",
+                local_path = "./www/data/",
                 overwrite = TRUE,
                 dtoken = token)
   }
@@ -16,4 +16,4 @@ updatePrices <- function()  {
 
 
 ###LOAD###
-load("www/data/prices.RData") 
+load("./www/data/prices.RData") 
