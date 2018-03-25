273de369b7373a4faaa8bfa4cdbe6a648be17b10
chadmulligan <jeremy.isnard@gmail.com>
2018-03-25 14:55:08 +0200

    ..

--- shinyApp/global.R
+++ shinyApp/global.R
@@ -6,7 +6,7 @@ updatePrices <- function()  {
   lastmodif <- format(file.info("/srv/shiny-server/www/data/prices.RData")$mtime,
                       format = "%Y-%m-%d")
   if(lastmodif != Sys.Date()) {
-  token = readRDS("~srv/shiny-server/www/data/token.RDS")
+  token = readRDS("/srv/shiny-server/www/data/token.RDS")
   drop_download(path = "btc-prices/prices.Rdata",
                 local_path = "/srv/shiny-server/www/data/",
                 overwrite = TRUE,
