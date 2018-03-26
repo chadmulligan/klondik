48cf70cb305d0c800038bd3c03a5289aa8a1d64c
chadmulligan <jeremy.isnard@gmail.com>
2018-03-26 07:26:39 +0200

    DLgraph + .rds

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -8,96 +8,98 @@ library(rdrop2)
 
 source("global.R")
 
-ui <- fluidPage(useShinyjs(), 
-  
+ui <- fluidPage(useShinyjs(),
+
   tags$head(tags$link(rel="shortcut icon", type = "image/png", href="favicon.png")),
-  
+
   tags$head(
     tags$style(HTML("hr {border-top: 1px solid;}"))
   ),
 
   titlePanel(img(src = "mail.png",
                  height = 113, width = 241),
-             windowTitle = "klondikBTC"),
-  
-  sidebarLayout(
-    
-    sidebarPanel(
-      
-      width = 3, 
+             windowTitle = "klondik.io - Bitcoin Compliance Toolbox"),
 
-      textInput(inputId = "address", 
+  sidebarLayout(
+
+    sidebarPanel(
+
+      width = 3,
+
+      textInput(inputId = "address",
                 label = "Enter a BTC address",
                 value = "",
                 width = "90%",
                 placeholder = "13aK3ydezpfdwQDB34M7dcRugg8XfhQaEE"),
-      
-      fileInput(inputId = "file1", 
+
+      fileInput(inputId = "file1",
                 label = "Or upload a list of addresses (*.csv )",
-                width = "90%", 
+                width = "90%",
                 accept = c('text/csv',
                            'text/comma-separated-values',
                            '.csv')
       ),
-      
-        
-      actionButton(inputId = "getinfo", 
+
+
+      actionButton(inputId = "getinfo",
                      label = "New Search"),
       helpText("Prices in USD are based on daily closing price"),
       hr(),
-      
-      actionButton(inputId = "append", 
+
+      actionButton(inputId = "append",
                    label = "Add Search"),
       helpText("Append new address transactions to results"),
       hr(),
-      
+
       actionButton(inputId = "getcluster",
                    label = "Cluster"),
       helpText("Look for parent addresses from the results"),
       hr(),
-      
+
       actionButton(inputId = "getscam",
                    label = "Scam Report"),
       helpText("Check whether the addresses were reported for scam"),
       hr(),
-      
-      actionButton(inputId = "getgraph", 
+
+      actionButton(inputId = "getgraph",
                    label = "Graph Transactions"),
+
+      uiOutput("DLGraph"),
       hr(),
-      
+
       downloadButton(outputId = "downloadCSV",
                      label = "Download Transactions")
-      
+
     ),
-    
-  
+
+
     mainPanel(
-      
-      width = 9, 
-      
+
+      width = 9,
+
       tabsetPanel(id = "nav",
-                  type = "pills", 
+                  type = "pills",
                   tabPanel("Transactions",
                            style = "overflow-y:scroll; max-height: 750px",
                            DTOutput("transactions")),
-                  tabPanel("Cluster", 
+                  tabPanel("Cluster",
                            style = "overflow-y:scroll; max-height: 750px",
                            verbatimTextOutput("cluster"),
                            uiOutput("getclusterinfo")),
-                  tabPanel("Scam Report", 
+                  tabPanel("Scam Report",
                            style = "overflow-y:scroll; max-height: 750px",
-                           value = "scamnull", 
+                           value = "scamnull",
                            verbatimTextOutput(outputId = "scamtext")),
-                  tabPanel("Scam Report", 
+                  tabPanel("Scam Report",
                            style = "overflow-y:scroll; max-height: 750px",
-                           value = "scamDT", 
-                           
+                           value = "scamDT",
+
                            uiOutput("scam")),
-                  tabPanel("All Addresses", 
+                  tabPanel("All Addresses",
                            style = "overflow-y:scroll; max-height: 750px",
                            verbatimTextOutput("allAddresses")),
-                  tabPanel("Graph", forceNetworkOutput("d3", height = "750px")), 
-                  tabPanel("About", 
+                  tabPanel("Graph", forceNetworkOutput("d3", height = "750px")),
+                  tabPanel("About",
                            style = "overflow-y:scroll; max-height: 750px",
                            includeMarkdown("www/About.md"))
                   )
@@ -107,54 +109,61 @@ ui <- fluidPage(useShinyjs(),
 
 
 server <- function(input, output, session) {
-  
+
   #session$onSessionEnded(stopApp)
-  
-  updatePrices()
-  
-  hideTab(inputId = "nav", 
+  session$allowReconnect(TRUE)
+
+  tryCatch(
+    {updatePrices()},
+    error = function(e){
+      showNotification(
+        ui = "Failed to update prices",
+        type = "error")
+    })
+
+  hideTab(inputId = "nav",
           target = "Cluster")
-  
-  hideTab(inputId = "nav", 
+
+  hideTab(inputId = "nav",
           target = "scamnull")
-  
-  hideTab(inputId = "nav", 
+
+  hideTab(inputId = "nav",
           target = "scamDT")
-  
-  hideTab(inputId = "nav", 
+
+  hideTab(inputId = "nav",
           target = "All Addresses")
-  
-  hideTab(inputId = "nav", 
+
+  hideTab(inputId = "nav",
           target = "Transactions")
-  
-  hideTab(inputId = "nav", 
+
+  hideTab(inputId = "nav",
           target = "Graph")
-  
+
   values <- reactiveValues()
-  
-  values$countfiles <- 0 
-  
+
+  values$countfiles <- 0
+
 
 ###OUTPUTS###
   observeEvent(input$getinfo, {
-      
+
     tryCatch({
-      
+
       if(isTruthy(input$address)) {
-        
+
         values$transactions <- getBTC(input$address)
         values$allAddresses <- input$address
-        values$searched <- input$address 
+        values$searched <- input$address
         reset("address")
-        
+
         } else{
-        
+
         if(!isTruthy(input$file1)){
-          
+
           warning()
-          
+
           } else {
-            
+
             values$f <- c(unlist(read.csv(input$file1[values$countfiles+1, "datapath"],
                                           header = FALSE,
                                           stringsAsFactors = FALSE),
@@ -163,14 +172,14 @@ server <- function(input, output, session) {
             values$transactions <- getBTC(values$f)
             values$allAddresses <- values$f
             values$searched <- values$f
-            
-            values$countfile <- values$countfile +1 
+
+            values$countfile <- values$countfile +1
             reset("file1")
-                
+
            }
         }
 
-      
+
       output$transactions <- renderDT({
       formatCurrency(DT::datatable(values$transactions,
                                    options = list(
@@ -180,19 +189,19 @@ server <- function(input, output, session) {
                                    )),
                      columns = "USD")
         })
-      
-      
-      
+
+
+
       showTab(inputId = "nav",
-              target = "Transactions", 
+              target = "Transactions",
               select = TRUE)
-      
-      
-      output$allAddresses <- renderText({paste(values$allAddresses, 
+
+
+      output$allAddresses <- renderText({paste(values$allAddresses,
                                                collapse = "\n")})
-      
+
       showTab(inputId = "nav",
-              target = "All Addresses", 
+              target = "All Addresses",
               select = FALSE)
 
       removeUI(selector = "#clusterinfobutton")
@@ -212,28 +221,28 @@ server <- function(input, output, session) {
           type = "error")}
       )
   })
-  
 
-  
+
+
   observeEvent(input$append, {
-    
+
     tryCatch({
-     
-      if (is.null(values$searched)) message() else{   
+
+      if (is.null(values$searched)) message() else{
         if (input$address %in% values$searched) warning() else{
           values$transactions <- distinct(rbind(getBTC(input$address), values$transactions))
           values$allAddresses <- unique(c(input$address, values$allAddresses))
-          output$allAddresses <- renderText({paste(values$allAddresses, 
+          output$allAddresses <- renderText({paste(values$allAddresses,
                                                    collapse = "\n")})
-    
+
           updateTabsetPanel(session,
-                            inputId = "nav", 
+                            inputId = "nav",
                             selected = "Transactions")
-          
+
           showTab(inputId = "nav",
                   target = "All Addresses",
                   select = FALSE)
-          
+
           values$searched <- c(values$searched, input$address)
         }
       }
@@ -255,114 +264,113 @@ server <- function(input, output, session) {
       }
     )
   })
-  
-  
-  
+
+
+
   observeEvent(input$getcluster, {
-    
+
     tryCatch({
-      #if is.null(values$cluster) message("already clustered)
       values$cluster <- cluster(values$transactions, values$allAddresses)
-    
+
       values$allAddresses <- unique(c(values$allAddresses, values$cluster))
-      
-      output$cluster <- renderText({paste(values$cluster, 
+
+      output$cluster <- renderText({paste(values$cluster,
                                          collapse = "\n")})
-      
-      output$allAddresses <- renderText({paste(values$allAddresses, 
+
+      output$allAddresses <- renderText({paste(values$allAddresses,
                                               collapse = "\n")})
-      
+
       showTab(inputId = "nav",
-              target = "Cluster", 
+              target = "Cluster",
               select = TRUE)
-        
+
       showTab(inputId = "nav",
-              target = "All Addresses", 
+              target = "All Addresses",
               select = FALSE)
-      
+
       output$getclusterinfo <- renderUI({
         actionButton(inputId = "clusterinfobutton",
                           label = "Get new transactions")
         })
-      
-    }, 
+
+    },
     error = function(err){
       showNotification(
         ui = paste("No parent address found"),
         type = "message")
     })
-    
+
   })
 
-  
-  
+
+
   observeEvent(input$clusterinfobutton, {
 
     values$transactions <- distinct(rbind(getBTC(values$cluster), values$transactions))
-    
+
     values$searched <- c(values$searched, values$cluster)
-    
+
     values$cluster <- c()
-    
+
     updateTabsetPanel(session,
-                      inputId = "nav", 
+                      inputId = "nav",
                       selected = "Transactions")
-    
+
     reset("address")
-    
+
     removeUI(selector = "#clusterinfobutton")
-    
+
     })
-  
-  
-  
+
+
+
   observeEvent(input$getscam, {
-    
+
     tryCatch({
-      
+
       if(is.null(values$allAddresses)) {
-        
+
         message()
-        
-      } else { 
-      
-        index <- values$searched %in% values$scamAddresses 
-        
+
+      } else {
+
+        index <- values$searched %in% values$scamAddresses
+
         if(all(index)) {
-          
+
           warning()
-          
-          } else { 
-            
-            values$scamResults <- rbind(get.scam(values$searched[!index]), values$scamResults) 
+
+          } else {
+
+            values$scamResults <- rbind(get.scam(values$searched[!index]), values$scamResults)
             values$scamAddresses <- c(values$searched[!index], values$scamAddresses)
-            
+
 
             if(is.null(values$scamResults)) {
-              
+
               hideTab(inputId = "nav",
                       target = "scamDT")
-              
-              output$scamtext <- renderText("None of the addresses have been reported for scam.")      
-           
+
+              output$scamtext <- renderText("None of the addresses have been reported for scam.")
+
               showTab(inputId = "nav",
                       target = "scamnull",
                       select = TRUE)
-              
+
               } else {
-              
+
                 hideTab(inputId = "nav",
                         target = "scamnull")
-                  
+
                 output$scamresultDT <- renderDT(DT::datatable(values$scamResults))
-                
+
                 output$scam <- renderUI(DTOutput("scamresultDT"))
-                
+
                 showTab(inputId = "nav",
                         target = "scamDT",
                         select = TRUE)
-                  
-              }    
+
+              }
           }
         }
       },
@@ -376,26 +384,53 @@ server <- function(input, output, session) {
           ui = paste("Please start a New Search first"),
           type = "message")
       })
-    
+
     })
-  
-  
-  
+
+
+
   observeEvent(input$getgraph, {
-    
-    values$graph <- graphD3(df = values$transactions, 
-                            addresses = values$allAddresses)
-    
-    output$d3 <- renderForceNetwork(values$graph)
-    
-    showTab(inputId = "nav",
-            target = "Graph",
-            select = TRUE)
-    
+
+    tryCatch({
+      values$graph <- graphD3(df = values$transactions,
+                              addresses = values$allAddresses)
+
+      output$d3 <- renderForceNetwork(values$graph)
+
+      showTab(inputId = "nav",
+              target = "Graph",
+              select = TRUE)
+
+
+      output$DLGraph <- renderUI({
+        tags$br(
+          downloadButton(outputId = "downloadgraph",
+                       label = "Download Graph")
+          )
+        })
+      },
+      warning = function(w) {
+        showNotification(
+          ui = paste("Please start a New Search first."),
+          type = "warning")
+      }
+    )
   })
-  
-  
-  
+
+
+
+  output$downloadgraph <-  downloadHandler(
+    filename = function() {
+      paste(paste(values$searched[[1]]), ".html", sep = "")
+    },
+    content = function(file) {
+      saveNetwork(values$graph,
+                  file = file,
+                  selfcontained = TRUE)}
+    )
+
+
+
   output$downloadCSV <- downloadHandler(
     filename = function() {
       paste(paste(values$searched[[1]]), ".csv", sep = "")
@@ -404,7 +439,7 @@ server <- function(input, output, session) {
       write.csv(as.data.frame(c(values$transactions)), file)
     }
   )
-  
+
 }
 
 
--- shinyApp/global.R
+++ shinyApp/global.R
@@ -3,12 +3,12 @@
 ###UPDATING THE PRICES DF###
 updatePrices <- function()  {
   
-  lastmodif <- format(file.info("/srv/shiny-server/www/data/prices.RData")$mtime,
+  lastmodif <- format(file.info("www/data/prices.RData")$mtime,
                       format = "%Y-%m-%d")
   if(lastmodif != Sys.Date()) {
-  token = readRDS("/srv/shiny-server/www/data/token.RDS")
+  token = readRDS("www/data/token.rds")
   drop_download(path = "btc-prices/prices.Rdata",
-                local_path = "/srv/shiny-server/www/data/",
+                local_path = "www/data/",
                 overwrite = TRUE,
                 dtoken = token)
   }
@@ -16,6 +16,7 @@ updatePrices <- function()  {
 
 
 ###LOAD###
-load("/srv/shiny-server/www/data/prices.RData") 
+load("www/data/prices.RData") 
+#load("/srv/shiny-server/www/data/prices.RData") 
 
 apikey <- "c6b6a8d1-ec54-442f-9816-85b7dd1db06d"
