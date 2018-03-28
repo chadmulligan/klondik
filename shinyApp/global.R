3e69522660f135ee614c91d94804f0a00577f8b0
chadmulligan <jeremy.isnard@gmail.com>
2018-03-28 09:35:48 +0200

    /srv/etc.

--- shinyApp/global.R
+++ shinyApp/global.R
@@ -3,12 +3,12 @@
 ###UPDATING THE PRICES DF###
 updatePrices <- function()  {
   
-  lastmodif <- format(file.info("www/data/prices.RData")$mtime,
+  lastmodif <- format(file.info("/srv/shiny-server/www/data/prices.RData")$mtime,
                       format = "%Y-%m-%d")
   if(lastmodif != Sys.Date()) {
-  token = readRDS("www/data/token.rds")
+  token = readRDS("/srv/shiny-server/www/data/token.rds")
   drop_download(path = "btc-prices/prices.Rdata",
-                local_path = "www/data",
+                local_path = "/srv/shiny-server/www/data",
                 overwrite = TRUE,
                 dtoken = token)
   }
@@ -16,7 +16,7 @@ updatePrices <- function()  {
 
 
 ###LOAD###
-load("www/data/prices.RData") 
+load("/srv/shiny-server/www/data/prices.RData") 
 #load("/srv/shiny-server/www/data/prices.RData") 
 
 apikey <- "c6b6a8d1-ec54-442f-9816-85b7dd1db06d"
